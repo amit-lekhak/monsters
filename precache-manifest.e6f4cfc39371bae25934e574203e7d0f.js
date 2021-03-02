ab753db32bdf362c1997af6296f39f6d851c40f8
amit-lekhak <amitlekhak1@gmail.com>
2021-03-02 17:35:47 +0545

    Updates

---
+++ precache-manifest.e6f4cfc39371bae25934e574203e7d0f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21a78af2988bdec7ca813fac0d14ff30",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "c876b9e046db45c2f8d1",
+    "url": "/monsters/static/css/main.62d87302.chunk.css"
+  },
+  {
+    "revision": "78adac64eaa07ff35dea",
+    "url": "/monsters/static/js/2.356d906f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters/static/js/2.356d906f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c876b9e046db45c2f8d1",
+    "url": "/monsters/static/js/main.9992501e.chunk.js"
+  },
+  {
+    "revision": "3dab8aa598718cb8e8b3",
+    "url": "/monsters/static/js/runtime-main.f48c3991.js"
+  }
+]);
